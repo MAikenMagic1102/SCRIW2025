98924a7094ba487c19d2ef5172d4c5e77c78c6ae
James Rittgers <JamesR2010@protonmail.com>
2025-08-21 20:29:47 -0400

    Added future methods

--- src/main/java/frc/robot/subsystems/Elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator/Elevator.java
@@ -1,5 +1,15 @@
 package frc.robot.subsystems.Elevator;
 
-public class Elevator {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+public class Elevator extends SubsystemBase{
+
+    public void elevatorGroundIntake() {};
+
+    public void elevatorLowerReef() {};
+
+    public void elevatorUpperReef() {};
+
+    public void elevatorHome() {};
+    
 }
