b4695dec534a00ab9c59c0d16edbf299f74c252a
James Rittgers <JamesR2010@protonmail.com>
2025-08-26 19:39:34 -0400

    Update Elevator.java

--- src/main/java/frc/robot/subsystems/Elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator/Elevator.java
@@ -1,5 +1,29 @@
 package frc.robot.subsystems.Elevator;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.Slot1Configs;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
-public class Elevator {
 
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
+
+    public void elevatorGroundIntake(){
+        // Move the elevator to da setpoint
+    };
+
+    public void elevatorLowerReef() {};
+
+    public void elevatorUpperReef() {};
+
+    public void elevatorHome() {};
+
+    public void elevatorScore() {};
+    
 }
