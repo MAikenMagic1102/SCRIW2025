986f35bfe26a102da92f2263eae91d237a53b84f
NeoLoonz <madisonlunna@icloud.com>
2025-08-26 20:46:51 -0400

    Elevator Constants
    Elevator set points.

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
@@ -1,5 +1,57 @@
 package frc.robot.subsystems.Elevator;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.CANcoderSimState;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 public class ElevatorConstants {
+
+final TalonFX m_motorLeft = new TalonFX(0);
+final TalonFX m_motorRight = new TalonFX(1);
+private final CANcoder leftSensor = new CANcoder(0);
+private final CANcoder rightSensor = new CANcoder(1);
+
+final DutyCycleOut m_motorleft = new DutyCycleOut(0.0);
+final DutyCycleOut m_motorright = new DutyCycleOut(0.0);
+
+private final TalonFXSimState leftSim = m_motorLeft.getSimState();
+private final TalonFXSimState rightSim = m_motorRight.getSimState();
+
+public static final class ElevatorSetpoints {
+    public static final int barge = 0;
+    public static final int processor = 0;
+    public static final int elevatorGroundIntake  = 0;
+    public static final int elevatorLowerReef = 0;
+    public static final int elevatorUpperReef = 0;
+    public static final int elevatorHome = 0;
+    public static final int elevatorscore = 0;
+}
+
+public static final class SimulationRobotConstants {
+
+    public static final double MinElevatorHeightMeters = 0; // m
+    public static final double MaxElevatorHeightMeters = 0; // m
+
+
+}
+
+
+// private final CANcoderSimState leftSensSim = m_motorLeft.getSimState();
+// private final CANcoderSimState rightSensSim = m_motorRight.getSimState();
+
+
+    // private final Pigeon2 imu = new Pigeon2(0);
+
+    // private final TalonFXSimState leftSim = leftFX.getSimState();
+    // private final TalonFXSimState rightSim = rightFX.getSimState();
+    // private final CANcoderSimState leftSensSim = leftSensor.getSimState();
+    // private final CANcoderSimState rightSensSim = rightSensor.getSimState();
 
 }
