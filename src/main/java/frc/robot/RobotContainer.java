353c3a9d79a0d65dbfb0cd28c6ab0cf2290a31fb
NeoLoonz <madisonlunna@icloud.com>
2025-10-02 19:31:11 -0400

    AutoMove
    Corrected where auto was, changed while to on, and got rid of instant command for intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,6 +27,7 @@ import frc.robot.subsystems.superStructure.SuperStructure;
 
 public class RobotContainer {
 
+    
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
@@ -77,7 +78,8 @@ public class RobotContainer {
         
         joystick.x().onTrue(superStructure.groundIntakeCommand);
         joystick.y().whileTrue(superStructure.reefHighCommand);
-        joystick.rightBumper().whileTrue(superStructure.reefLowCommand);
+        joystick.rightBumper().onTrue(superStructure.reefLowCommand);
+
         joystick.rightTrigger().whileTrue(superStructure.processorScoreCommand);
         joystick.leftTrigger().whileTrue(superStructure.bargeScoreCommand);
         // TODO: fix ur friggin keybinds to make dem les scuffed and les conflicty
@@ -95,11 +97,14 @@ public class RobotContainer {
         drivetrain.registerTelemetry(logger::telemeterize);
       
       
-              joystick.x().onTrue(new InstantCommand(()-> Intake.intakeIn()));
-            }
+            //   joystick.x().onTrue(new InstantCommand(()-> Intake.intakeIn()));
+            // }
 
-            public Command getAutonomousCommand() {
-                return Commands.print("No autonomous command configured");
-            }
+     
         }
+        
+        public Command getAutonomousCommand() {
+            return Commands.print("No autonomous command configured");
+        }
+    }
 
