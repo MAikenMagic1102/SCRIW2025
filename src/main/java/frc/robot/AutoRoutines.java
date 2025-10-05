8a7cd05faa9f12b69fc9552387407f9a34efc329
NeoLoonz <madisonlunna@icloud.com>
2025-10-05 12:10:38 -0400

    AHHHHHHHHHh

---
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -0,0 +1,41 @@
+package frc.robot;
+
+import java.util.function.Supplier;
+
+import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+// import frc.robot.commands.AutoRunToCoral;
+// import frc.robot.commands.DriveToPoseCommand;
+// import frc.robot.commands.IntakeDeploy;
+// import frc.robot.commands.IntakeRetract;
+// import frc.robot.commands.IntakeRollersOn;
+// import frc.robot.commands.PrepScore;
+// import frc.robot.commands.ReturnToHome;
+// import frc.robot.commands.ScoreCoral;
+// import frc.robot.field.FieldUtils;
+// import frc.robot.field.ReefPole;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+// import frc.robot.subsystems.Superstructure;
+// import frc.robot.subsystems.CoralGripper.CoralGripper;
+// import frc.robot.subsystems.Intake.CoralIntake;
+// import frc.robot.util.PoseUtils;
+
+
+public class AutoRoutines {
+
+    private final AutoFactory m_factory;
+    private final CommandSwerveDrivetrain drive;
+    // public AutoRoutine Red_Mid_Frwd(){
+
+    // }    
+    
+}
