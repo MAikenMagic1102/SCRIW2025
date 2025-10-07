a5674575e0b4780890725cc90fe681105acd7b24
NeoLoonz <madisonlunna@icloud.com>
2025-10-06 20:51:10 -0400

    Pivot
    Basically a complete re-write, based off of REEFSCAPE2025, with variable changes. There was no pivot in 2025, so its based off the arms code. So future updates of this code will be less based off the arm.

--- src/main/java/frc/robot/subsystems/Pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot/Pivot.java
@@ -1,79 +1,171 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.Pivot;
 
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import org.littletonrobotics.junction.Logger;
+
+import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.sim.CANcoderSimState;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Elevator.ElevatorConstants;
 
+public class Pivot extends SubsystemBase {
+  private TalonFX armMotor;
+  private TalonFXSimState motorSim;
+  private CANcoderSimState cancoderSim;
 
-public class Pivot extends SubsystemBase{ 
+  private CANcoder armCaNcoder;
 
-    private final TalonFX motorPivot;
-    private final DutyCycleOut motorPivotRequest = new DutyCycleOut(0.0);
-    private final PositionVoltage motorPositionRequest = new PositionVoltage(0.0);
+  private DCMotor armGearbox = DCMotor.getKrakenX60(1);
 
-    private StatusSignal<Angle> rotorPosSignal;
-    private Angle rotorPos;
+  private SingleJointedArmSim armSim = 
+    new SingleJointedArmSim(
+      armGearbox,
+      PivotConstants.armGearing,
+      SingleJointedArmSim.estimateMOI(PivotConstants.armLength, PivotConstants.armMass),
+      PivotConstants.armLength,
+      PivotConstants.armMinAngle,
+      PivotConstants.armMaxAngle,
+      true,
+      PivotConstants.armStartingAngle);
 
-    public double homeScoreAngle = 0;
-    public double reefIntakeAngle = 0;
-    public double currentAngle;
+  private DutyCycleOut dutyOut = new DutyCycleOut(0);
+  private PositionVoltage posVoltage = new PositionVoltage(0).withSlot(0);
+  private MotionMagicVoltage mmVoltage = new MotionMagicVoltage(0).withSlot(0);
 
-    public Pivot(){
-      motorPivot = new TalonFX(PivotConstants.pivot_ID, "rio");
-      TalonFXConfiguration motor_Config = new TalonFXConfiguration();
-      
-      //This has to be tuned for position control
-      motor_Config.Slot0.kP = 0;
-      //Set this correctly to make up positive and down negative
-      motor_Config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-      motor_Config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-      //Set this to gearing
-      motor_Config.Feedback.SensorToMechanismRatio = 0.0;
-      motorPivot.getConfigurator().apply(motor_Config);
-      rotorPosSignal = motorPivot.getRotorPosition();
-      rotorPos = rotorPosSignal.getValue();
-      currentAngle = rotorPosSignal.getValueAsDouble();
+  boolean closedLoop = false;
+
+  private double targetPosition = 0;
+
+  // AdvantageScope log paths
+  private final String loggerPath = "Subsystems/Pivot";
+  private final String motorLoggerPath = loggerPath + "/Motor";
+/** Creates a new Arm. */
+  public Pivot() {
+    armMotor = new TalonFX(PivotConstants.motorID, PivotConstants.busname);
+    armCaNcoder = new CANcoder(PivotConstants.cancoderID, PivotConstants.busname);
+
+    /* Retry config apply up to 5 times, report if failure */
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; ++i) {
+      status = armMotor.getConfigurator().apply(PivotConstants.config);
+      if (status.isOK()) break;
+    }
+    if(!status.isOK()) {
+      System.out.println("Could not apply configs, error code: " + status.toString());
     }
 
-    public void setPosition(double position){
-      motorPivot.setControl(motorPositionRequest.withPosition(position));
+    /* Retry config apply up to 5 times, report if failure */
+    StatusCode ccstatus = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; ++i) {
+      ccstatus = armCaNcoder.getConfigurator().apply(PivotConstants.ccconfig);
+      if (ccstatus.isOK()) break;
+    }
+    if(!ccstatus.isOK()) {
+      System.out.println("Could not apply configs, error code: " + ccstatus.toString());
     }
 
-    public Command groundIntake() {
-        while (currentAngle >= 211 && currentAngle <= 209) {
-            return Commands.runOnce(()-> motorPivot.setControl(motorPivotRequest.withOutput(1)));
-        }
-        return Commands.none();
+    //armMotor.setPosition(armCaNcoder.getAbsolutePosition().getValueAsDouble() / ArmConstants.armRotorToSensor);
+    motorSim = armMotor.getSimState();
+    cancoderSim = armCaNcoder.getSimState();
+    armCaNcoder.setPosition(0);
+  }
+
+  @Override
+  public void periodic() {
+
+    if(armAtScoring()){
+      PivotConstants.driveSpeed = 0.15;
+    }else{
+      PivotConstants.driveSpeed = 1.0;
     }
+    // This method will be called once per scheduler run
+
+
+    // Logging
+    Logger.recordOutput(loggerPath + "/Angle", getAngleDegrees());
+    Logger.recordOutput(loggerPath + "/At Goal", atGoal());
+
+    Logger.recordOutput(motorLoggerPath + "/Motor Voltage", armMotor.getMotorVoltage().getValueAsDouble());
+    Logger.recordOutput(motorLoggerPath + "/Stator Current", armMotor.getStatorCurrent().getValueAsDouble());
+    Logger.recordOutput(motorLoggerPath + "/Motor Temp", armMotor.getDeviceTemp().getValueAsDouble());
     
-    public Command reefIntake() {
-        while (currentAngle >= 121 && currentAngle <= 119) {
-            return Commands.runOnce(()-> motorPivot.setControl(motorPivotRequest.withOutput(1)));
-        }
-        return Commands.none();  // does nothing; satisfies the compiler
+    if(closedLoop){
+      Logger.recordOutput("Arm/ Setpoint", armMotor.getClosedLoopReference().getValueAsDouble());
     }
+  }
 
-    public Command homeScore() {
-        while (currentAngle >= 1 && currentAngle <= -1) {
-            return Commands.runOnce(()-> motorPivot.setControl(motorPivotRequest.withOutput(1)));
-        }
-        return Commands.none();
-    }
+  public boolean armHalfScored(){
+    return getAngleDegrees() < -100;
+  }
 
-    @Override
-    public void periodic(){
-        rotorPosSignal = motorPivot.getRotorPosition();
-        rotorPos = rotorPosSignal.getValue();
-        currentAngle = rotorPosSignal.getValueAsDouble();
-    }
-   
-};
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run
+    motorSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+    cancoderSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+    
+    armSim.setInput(motorSim.getMotorVoltage());
+
+    armSim.update(0.020);
+
+    RoboRioSim.setVInVoltage(
+        BatterySim.calculateDefaultBatteryLoadedVoltage(armSim.getCurrentDrawAmps()));
+
+    motorSim.setRawRotorPosition(Units.radiansToRotations(armSim.getAngleRads()) * PivotConstants.armGearing);
+    cancoderSim.setRawPosition(Units.radiansToRotations(armSim.getAngleRads()) * PivotConstants.armGearingCANcoder);
+  }
+
+  public double getAngleDegrees(){
+    return Units.rotationsToDegrees(armCaNcoder.getPosition().getValueAsDouble() / PivotConstants.armGearingCANcoder);
+  }
+
+  public boolean armAtScoring(){
+    return getAngleDegrees() < -180;
+  }
+
+  public boolean armAtHome(){
+    return getAngleDegrees() > -8 && getAngleDegrees() < 8;
+  }
+  
+  public boolean atGoal(){
+    return Math.abs(targetPosition - getAngleDegrees()) < PivotConstants.positionTolerence;
+  }
+
+  public void setOpenLoop(double demand){
+    dutyOut.withOutput(demand);
+    armMotor.setControl(dutyOut);
+    closedLoop = false;
+  }
+
+  public void setAnglePosition(double angle){
+    targetPosition = angle;
+    posVoltage.withPosition(Units.degreesToRotations(angle));
+    armMotor.setControl(posVoltage);
+    closedLoop = true;
+  }
+
+  public Command setAngle(double angle){
+    return runOnce(() -> setAnglePosition(angle));
+  }
+  
+
+}
--- src/main/java/frc/robot/subsystems/Pivot/PivotConstants.java
+++ src/main/java/frc/robot/subsystems/Pivot/PivotConstants.java
@@ -4,7 +4,75 @@
 
 package frc.robot.subsystems.Pivot;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+import edu.wpi.first.math.util.Units;
+
 /** Add your docs here. */
 public class PivotConstants {
-    public static final int pivot_ID = 40;
+    public static String busname = "rio";
+    public static int motorID = 40;
+    public static int cancoderID = 311;
+
+    public static double armGearing = 89.83;
+    public static double armGearingCANcoder = 3.5;
+    public static double armRotorToSensor = 25.67;
+    public static double armLength = Units.inchesToMeters(25);
+    public static double armMass = Units.lbsToKilograms(5.0);
+    public static double armMinAngle = Units.degreesToRadians(-255.0);
+    public static double armMaxAngle = Units.degreesToRadians(255.0);
+    public static double armStartingAngle = Units.degreesToRadians(0.0);
+
+    public static double positionTolerence = 7.0;
+
+    public static double driveSpeed = 1.0;
+
+    public static double lowerReef = -275; //85;
+    public static double upperReef = -256; //110;
+    public static double ALGAE = -245;
+
+    public static TalonFXConfiguration config = new TalonFXConfiguration()
+        .withCurrentLimits(
+            new CurrentLimitsConfigs()
+                .withSupplyCurrentLimit(70)
+
+        )  
+        .withMotorOutput(
+            new MotorOutputConfigs()
+                .withNeutralMode(NeutralModeValue.Brake)
+          )   
+          //10:70 - 18:66 S - 10:35
+        .withFeedback(
+            new FeedbackConfigs()
+                .withSensorToMechanismRatio(3.5)
+                // .withFeedbackRemoteSensorID(cancoderID)
+                .withRotorToSensorRatio(25.67)
+                .withFeedbackSensorSource(FeedbackSensorSourceValue.FusedCANcoder)
+        )
+        .withSlot0(
+            new Slot0Configs()
+                .withKG(0.38)
+                .withKD(0)
+                .withKP(60.0)
+                .withKV(0.5)
+        );
+
+    public static CANcoderConfiguration ccconfig = new CANcoderConfiguration()
+        .withMagnetSensor(
+            new MagnetSensorConfigs()
+                .withAbsoluteSensorDiscontinuityPoint(0.0)
+                .withMagnetOffset(-0.87)
+                //.withMagnetOffset(1.32)
+                .withSensorDirection(SensorDirectionValue.CounterClockwise_Positive)
+        );
+
 }
